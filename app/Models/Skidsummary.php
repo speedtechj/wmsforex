7ac8f771d52aeff49b66aad7e982777f44442b52
speedtechj <arbiasjr@gmail.com>
2024-05-04 11:50:37 -0600

    update

--- app/Models/Skidsummary.php
+++ app/Models/Skidsummary.php
@@ -9,7 +9,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 class Skidsummary extends Model
 {
     use HasFactory;
-    protected $table = 'Skidweights';
+    protected $table = 'skidweights';
     protected $guarded = [];
     public function booking() : BelongsTo
     {
