ef481c2d5d3d87234b3c44efb117d5b92111bf62
speedtechj <arbiasjr@gmail.com>
2024-05-09 12:30:47 -0600

    update

--- app/Filament/Resources/SkidmanifestResource/Pages/ListSkidmanifests.php
+++ app/Filament/Resources/SkidmanifestResource/Pages/ListSkidmanifests.php
@@ -13,7 +13,7 @@ class ListSkidmanifests extends ListRecords
     protected function getHeaderActions(): array
     {
         return [
-            Actions\CreateAction::make(),
+            // Actions\CreateAction::make(),
         ];
     }
 }
