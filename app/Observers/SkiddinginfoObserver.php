3a1269d67621c3d54bd6ddba8f130258243a3cbd
speedtechj <arbiasjr@gmail.com>
2024-05-13 13:47:01 -0600

    update

--- app/Observers/SkiddinginfoObserver.php
+++ app/Observers/SkiddinginfoObserver.php
@@ -12,11 +12,20 @@ class SkiddinginfoObserver
      */
     public function created(Skiddinginfo $skiddinginfo): void
     {
-        $length = $skiddinginfo->booking->boxtype->lenght ?? 0;
-        $width = $skiddinginfo->booking->boxtype->width ?? 0;
-        $height = $skiddinginfo->booking->boxtype->height ?? 0;
-        $boxcbm = round($length * $width * $height / 61024, 2);
-        $skiddinginfo->update(['cbm' => $boxcbm]);
+        
+        if($skiddinginfo->booking->boxtype_id == 4){
+            $length = $skiddinginfo->booking->irregular_width;
+            $width = $skiddinginfo->booking->irregular_height;
+            $height = $skiddinginfo->booking->irregular_length;
+            $boxcbm = round($length * $width * $height / 61024, 2);
+            $skiddinginfo->update(['cbm' => $boxcbm]);
+        }else {
+            $length = $skiddinginfo->booking->boxtype->lenght ?? 0;
+            $width = $skiddinginfo->booking->boxtype->width ?? 0;
+            $height = $skiddinginfo->booking->boxtype->height ?? 0;
+            $boxcbm = round($length * $width * $height / 61024, 2);
+            $skiddinginfo->update(['cbm' => $boxcbm]);
+        }
     }
     /**
      * Handle the Skiddinginfo "updated" event.
