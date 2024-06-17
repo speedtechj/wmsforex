5da146921e1053a8d677b0ea7150b5b9ef18664f
speedtechj <arbiasjr@gmail.com>
2024-06-17 13:21:09 -0600

    update

--- app/Models/Scopes/InvloadedScope.php
+++ app/Models/Scopes/InvloadedScope.php
@@ -13,6 +13,6 @@ class InvloadedScope implements Scope
      */
     public function apply(Builder $builder, Model $model): void
     {
-        $builder->whereDate('booking_date', '>=' , now()->subDays(10));
+        $builder->whereDate('booking_date', '>=' , now()->subDays(15));
     }
 }
